72d06540074d8220864184da96dc51094d8a500b
gerater <sger0505@gmail.com>
2023-05-21 18:31:16 -0300

    Added the round function of the game

--- script.js
+++ script.js
@@ -6,9 +6,54 @@
 // 3) Crear la funcion game que permita jugar 5 rounds con un loop. conmsole log para mostrar los resultados de cada ronda y el ganador al final
 
 const rockPaperScissors = ["Rock", "Paper", "Scissors"];
-const computerSelection = rockPaperScissors[Math.floor(Math.random() * rockPaperScissors.length)];
-const playerSelection = insensitiveCase(prompt("Piedra, papel o tijeras?"));
+
+const win = "You won!",
+lose = "You lose.",
+draw = "Draw";
+
+
+
+
 function insensitiveCase(playerSelection){
     playerSelection = playerSelection.toLowerCase();
     return playerSelection;
+}
+
+function playRound(playerSelection, computerSelection){
+    computerSelection = rockPaperScissors[Math.floor(Math.random() * rockPaperScissors.length)];
+    playerSelection = insensitiveCase(prompt("Piedra, papel o tijeras?"));
+    if (playerSelection === "rock"){
+        if (computerSelection === "Rock"){
+            return draw;
+        } else if (computerSelection === "Paper"){
+            return lose;
+        } else{
+            return win;
+        } 
+    } else if(playerSelection === "paper"){
+        if (computerSelection === "Rock"){
+            return win;
+        } else if (computerSelection === "Paper"){
+            return draw;
+        } else{
+            return lose;
+        } 
+    } else{
+        if(computerSelection === "Rock") {
+            return lose;            
+        } else if(computerSelection === "Paper"){
+            return win;
+        } else{
+            return draw;
+        }
+    }    
+}
+
+console.log(playRound());
+
+
+const result = playRound();
+
+function game(result){
+
 }
